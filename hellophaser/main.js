0d3369b539f89285e07384d277d9c70e52451bbc
rratkevich <ratkevichigor94@gmail.com>
2019-12-18 09:44:49 +0300

    "add animation"

--- hellophaser/main.js
+++ hellophaser/main.js
@@ -15,16 +15,20 @@ function create() {
     this.brilliant.scale.setTo(0.4, 0.4);
     this.brilliant.animations.add('rotate', [1,2,3,4], 4, true);
     this.brilliant.animations.play('rotate');
-    // this.brilliant = game.add.tween(sprite).to({ x: 200, y:200 }, 10000, Phaser.Easing.Linear.None, true);
+
+
     this.middle = game.add.sprite(300,300, 'middle');
     this.middle.scale.setTo(0.4, 0.4);
     this.middle.animations.add('rotate', [1,2,3,4], 4, true);
     this.middle.animations.play('rotate');
+    this.middle = game.add.tween(sprite).to({ x: 450, y:450 }, 10000, Phaser.Easing.Linear.None, true);
+
 
     this.finish = game.add.sprite(50,50, 'finish');
     this.finish.scale.setTo(0.4, 0.4);
     this.finish.animations.add('rotate', [1,2,3,4], 4, true);
     this.finish.animations.play('rotate');
+    this.finish = game.add.tween(sprite).to({ x: 300, y:300 }, 10000, Phaser.Easing.Linear.None, true);
 }
 
 function update() {
